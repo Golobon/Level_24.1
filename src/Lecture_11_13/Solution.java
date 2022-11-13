335b9dc5ed8e41c3310e77d81a394a2740b9e5fc
Golobon <golobon@ya.ru>
2022-11-13 21:37:43 +0300

    Commit

--- src/Lecture_11_13/Solution.java
+++ src/Lecture_11_13/Solution.java
@@ -1,15 +1,16 @@
 package Lecture_11_13;
 
 import java.io.*;
+import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        Set<String> set = new TreeSet<>((o1, o2) -> {
-            int x1 = Integer.parseInt(o1.substring(o1.lastIndexOf(".part") + 5));
-            int x2 = Integer.parseInt(o2.substring(o2.lastIndexOf(".part") + 5));
-            return x1 - x2; });
+        Set<String> set = new TreeSet<>((o1,o2) -> {
+                int x1 = Integer.parseInt(o1.substring(o1.lastIndexOf(".part") + 5));
+                int x2 = Integer.parseInt(o2.substring(o2.lastIndexOf(".part") + 5));
+                return x1 - x2;  });
 
         try (BufferedReader bR = new BufferedReader(new InputStreamReader(System.in))) {
         while (true) { String path = bR.readLine();
@@ -19,8 +20,8 @@ public class Solution {
         File theFile = new File(set.stream().iterator().next());
 
         String directory = theFile.getParent();
-        String[] fileNameParts = theFile.getName().split("\\.");
-        String fileName = fileNameParts[0] + "." + fileNameParts[1];
+        String fileName = theFile.getName().replaceAll(".part.*", "");
+        System.out.println(fileName);
 
         String writeWay;
         if (directory == null) writeWay = fileName;
